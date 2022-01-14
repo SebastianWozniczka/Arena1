3d96af03d4a6d34cf489aef09ddf28cc2724ca66
SebastianWozniczka <49164201+SebastianWozniczka@users.noreply.github.com>
2022-01-14 18:46:53 +0100

    Small changes
    -Adding white hole
    -Adding player rotation
    -Adding run animation
    -Other small changes

--- Arena1/Assets/scripts/playerMove.cs
+++ Arena1/Assets/scripts/playerMove.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,28 +8,50 @@ public class playerMove : MonoBehaviour
 {
 
     private Rigidbody2D playerRigidbody2D;
+    private Animator animator;
     public Button left, right, up;
+    public SpriteRenderer spriteRenderer;
+    public Sprite sprite1,sprite2;
     public PolygonCollider2D polygonCollider2D;
+    public Transform transform;
+   
+
 
     private bool isLeft = false, isRight = false, isUp = false;
 
     private float moveSpeed = 2.0f;
     private float launchSpeed = 6.0f;
     private bool isGrounded;
+    private bool facingDirection;
+    private float timer,timer2;
+    public float inputX;
 
 
     void Start()
     {
         playerRigidbody2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
 
         left.onClick.AddListener(moveLeft);
         right.onClick.AddListener(moveRight);
         up.onClick.AddListener(jump);
+        animator.SetTrigger("run");
+        
+
+        timer = 0;
+        timer2 = 0;
+        
+
     }
 
   
     void Update()
     {
+       
+
+        playerRotate();
+
+
         if (isLeft)
         {
             playerRigidbody2D.AddForce(Vector2.left * moveSpeed, ForceMode2D.Impulse);
@@ -48,16 +71,83 @@ public class playerMove : MonoBehaviour
             isUp = false;
         }
 
+        if (playerRigidbody2D.velocity.x == 0)
+        {
 
-        Vector2[] vector2=new Vector2[5];
-        vector2[0] = new Vector2(- 0.02255917f, 0.05214572f);
-        vector2[1] = new Vector2(-4, 0.5f);
-        vector2[2] = new Vector2(-4, -0.5f);
-        vector2[3] = new Vector2(4, -0.5f);
-        vector2[4] = new Vector2(4, 0.5f);
+          
+            timer += Time.deltaTime;
+            if (timer > 2)
+            {
+                NewPoints();
+                spriteRenderer.sprite = sprite1;
 
-        polygonCollider2D.SetPath(0,vector2);
+                timer = 0;
+            }
+        }
+        else
+        {
+            timer = 0;
+           
+ 
+        }
+
+
+
+        
         Debug.Log(polygonCollider2D.points.GetValue(0));
+    }
+
+    void playerRotate()
+    {
+      
+        inputX = playerRigidbody2D.velocity.x;
+
+        if (inputX < 0 && !facingDirection)
+        {
+            timer2 += Time.deltaTime;
+            if (timer2 > 0.1f)
+            {
+                playerRigidbody2D.transform.Rotate(0, 180, 0);
+                facingDirection = true;
+                timer2 = 0;
+            }
+        }
+        else if (inputX > 0 && facingDirection)
+        {
+            timer2 += Time.deltaTime;
+            if (timer2 > 0.1f)
+            {
+                playerRigidbody2D.transform.Rotate(0, 180, 0);
+                facingDirection = false;
+                timer2 = 0;
+            }
+        }
+      
+
+
+
+
+
+    }
+
+    void NewPoints()
+    {
+
+
+        Vector2[] vector2 = new Vector2[10];
+        vector2[0] = new Vector2(0.36f, 0.4f);
+        vector2[1] = new Vector2(0.26f, 0.21f);
+        vector2[2] = new Vector2(0.09f, 0.12f);
+        vector2[3] = new Vector2(-0.12f, -0.12f);
+        vector2[4] = new Vector2(-0.33f, 0.25f);
+        vector2[5] = new Vector2(-0.4f, 0.46f);
+        vector2[6] = new Vector2(-4, 0.5f);
+        vector2[7] = new Vector2(-4, -0.5f);
+        vector2[8] = new Vector2(4, -0.5f);
+        vector2[9] = new Vector2(4, 0.5f);
+
+        polygonCollider2D.SetPath(0, vector2);
+
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
