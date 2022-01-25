5a0b9e574109abdbf5c5d2d47a4d8e8122e60437
SebastianWozniczka <49164201+SebastianWozniczka@users.noreply.github.com>
2022-01-25 21:07:49 +0100

    minor code fixes

--- Arena1/Assets/scripts/playerMove.cs
+++ Arena1/Assets/scripts/playerMove.cs
@@ -1,34 +1,79 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class playerMove : MonoBehaviour
+public class PlayerMove : MonoBehaviour
 {
 
     private Rigidbody2D playerRigidbody2D;
+    private Animator animator;
     public Button left, right, up;
+    public SpriteRenderer spriteRenderer;
+    public Sprite sprite1,sprite2,sprite3;
     public PolygonCollider2D polygonCollider2D;
+    public new Transform transform;
+    public AudioSource audioSource1;
 
+   // GameObject a;
+
+
+
+    public float posX;
+    private float y;
     private bool isLeft = false, isRight = false, isUp = false;
 
-    private float moveSpeed = 2.0f;
-    private float launchSpeed = 6.0f;
+    private readonly float moveSpeed = 2.0f;
+    private readonly float launchSpeed = 6.0f;
     private bool isGrounded;
+    private bool facingDirection;
+    private float timer,timer2,timer1b;
+    public float inputX;
 
 
     void Start()
     {
         playerRigidbody2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+ 
+        left.onClick.AddListener(MoveLeft);
+        right.onClick.AddListener(MoveRight);
+        up.onClick.AddListener(Jump);
+        animator.SetTrigger("stand");
+       
+        timer = 0;
+        timer2 = 0;
+        
 
-        left.onClick.AddListener(moveLeft);
-        right.onClick.AddListener(moveRight);
-        up.onClick.AddListener(jump);
     }
 
   
     void Update()
     {
+
+        
+       
+       
+        posX=playerRigidbody2D.position.x;
+
+        if (posX > 10)
+        {
+            playerRigidbody2D.transform.position = new Vector2(-10, playerRigidbody2D.position.y);
+        }
+
+        if (posX < -10)
+        {
+            playerRigidbody2D.transform.position = new Vector2(10, playerRigidbody2D.position.y);
+        }
+
+        PlayerRotate();
+
+        if (!isGrounded)
+        {
+            animator.SetTrigger("jump");
+        }
+        
         if (isLeft)
         {
             playerRigidbody2D.AddForce(Vector2.left * moveSpeed, ForceMode2D.Impulse);
@@ -48,21 +93,119 @@ public class playerMove : MonoBehaviour
             isUp = false;
         }
 
+        if (playerRigidbody2D.velocity.x < 1 && playerRigidbody2D.velocity.x > -1 && isGrounded)
+        {
 
-        Vector2[] vector2=new Vector2[5];
-        vector2[0] = new Vector2(- 0.02255917f, 0.05214572f);
+            animator.SetTrigger("stand");
+            timer += Time.deltaTime;
+            if (timer > 1)
+            { 
+                
+                y = -0.01f;
+                NewPoints(y);
+               
+                transform.position = new Vector2(posX, -3.6f);
+                spriteRenderer.sprite = sprite1;
+                audioSource1.Play();
+  
+            }
+
+            if(timer > 2)
+            {
+                //a = new GameObject("start");
+
+                y = -0.1f;
+                NewPoints(y);
+                spriteRenderer.sprite = sprite2;
+                if (facingDirection)
+                {
+                    spriteRenderer.sprite = sprite3;
+                    polygonCollider2D.enabled = false;
+                }
+            }
+
+           
+              
+
+          
+
+
+           
+
+
+
+
+        }
+
+        else
+        {
+            audioSource1.Pause();
+          
+            timer = 0;
+            if(isGrounded)
+            animator.SetTrigger("run");
+
+
+        }
+
+
+
+        
+        Debug.Log(polygonCollider2D.points.GetValue(0));
+    }
+
+    void PlayerRotate()
+    {
+      
+        inputX = playerRigidbody2D.velocity.x;
+
+        if (inputX < 0 && !facingDirection)
+        {
+            timer2 += Time.deltaTime;
+            if (timer2 > 0.2f)
+            {
+                playerRigidbody2D.transform.Rotate(0, 180, 0);
+                facingDirection = true;
+                timer2 = 0;
+            }
+        }
+        else if (inputX > 0 && facingDirection)
+        {
+            timer1b += Time.deltaTime;
+            if (timer1b > 0.2f)
+            {
+                playerRigidbody2D.transform.Rotate(0, 180, 0);
+                facingDirection = false;
+                timer1b = 0;
+            }
+        }
+      
+
+
+
+
+
+    }
+
+    void NewPoints(float y)
+    {
+
+
+        Vector2[] vector2 = new Vector2[5];
+        vector2[0] = new Vector2(posX+0.14f, y);
         vector2[1] = new Vector2(-4, 0.5f);
         vector2[2] = new Vector2(-4, -0.5f);
         vector2[3] = new Vector2(4, -0.5f);
         vector2[4] = new Vector2(4, 0.5f);
 
-        polygonCollider2D.SetPath(0,vector2);
-        Debug.Log(polygonCollider2D.points.GetValue(0));
+        polygonCollider2D.SetPath(0, vector2);
+
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         isGrounded = true;
+       
     }
 
     private void OnCollisionExit2D(Collision2D collision)
@@ -70,17 +213,17 @@ public class playerMove : MonoBehaviour
         isGrounded = false;
     }
 
-    private void moveLeft()
+    private void MoveLeft()
     {
         isLeft = true;
     }
 
-    private void moveRight()
+    private void MoveRight()
     {
         isRight = true;
     }
 
-    private void jump()
+    private void Jump()
     {
         isUp = true;
     }
